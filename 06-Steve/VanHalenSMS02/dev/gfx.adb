2a55f04a13e3f754fcc308fa6a405bfd0b0f8ea3
StevePro7 <steveprozune@hotmail.com>
2021-02-03 20:18:12 +0000

    no message

---
+++ 06-Steve/VanHalenSMS02/dev/gfx.adb
@@ -0,0 +1,8 @@
+M:gfx
+S:G$cursor__palette__bin$0$0({16}DA16d,SC:U),D,0,0
+S:G$cursor__tiles__psgcompr$0$0({187}DA187d,SC:U),D,0,0
+S:G$font__palette__bin$0$0({16}DA16d,SC:U),D,0,0
+S:G$font__tilemap__bin$0$0({128}DA128d,SC:U),D,0,0
+S:G$font__tiles__psgcompr$0$0({663}DA663d,SC:U),D,0,0
+S:G$sprite__palette__bin$0$0({16}DA16d,SC:U),D,0,0
+S:G$sprite__tiles__psgcompr$0$0({32}DA32d,SC:U),D,0,0
