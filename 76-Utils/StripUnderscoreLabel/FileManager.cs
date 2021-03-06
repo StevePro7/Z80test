ed21f8216afef131b902803aba3b840a8a9ea61f
StevePro7 <steveprozune@hotmail.com>
2021-03-06 15:43:36 +0000

    no message

---
+++ 76-Utils/StripUnderscoreLabel/FileManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace BinaryFileWrite
+{
+	public class FileManager
+	{
+		public void Process(string path)
+		{
+			Lines = File.ReadAllLines("input/" + path);
+		}
+
+		public void Saving(List<string> outLines, string path)
+		{
+			if (!Directory.Exists("output"))
+			{
+				Directory.CreateDirectory("output");
+			}
+
+			File.WriteAllLines("output/" + path, outLines.ToArray());
+		}
+
+		public string[] Lines { get; private set; }
+	}
+}
---
+++ 76-Utils/StripUnderscoreLabel/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace BinaryFileWrite
+{
+	class Program
+	{
+		static void Main()
+		{
+			var fileName = ConfigurationManager.AppSettings["fileName"];
+
+			Console.WriteLine("Stripping underscores:" );
+			Console.WriteLine($"{fileName}");
+			var fm = new FileManager();
+			fm.Process(fileName);
+			var inpLines = fm.Lines;
+			var outLines = new List<string>();
+
+			foreach(var inpLine in inpLines)
+			{
+				var txtLine = inpLine.TrimEnd();
+				if (txtLine.EndsWith(":"))
+				{
+					if (txtLine.StartsWith("_"))
+					{
+						txtLine = txtLine.Substring(1, txtLine.Length - 1);
+					}
+				}
+
+				outLines.Add(txtLine);
+			}
+
+			fm.Saving(outLines, fileName);
+			Console.WriteLine("Press [ RETURN ]");
+			Console.Read();
+		}
+
+	}
+}
