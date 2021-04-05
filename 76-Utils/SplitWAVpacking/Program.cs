673f30417b73fd04662550442aaa4ccdf2b6442c
StevePro7 <steveprozune@hotmail.com>
2021-04-05 18:29:09 +0100

    no message

--- 76-Utils/SplitWAVpacking/Program.cs
+++ 76-Utils/SplitWAVpacking/Program.cs
@@ -10,18 +10,19 @@ namespace SplitWAVpacking
 			cm.Init();
 			Console.WriteLine($"File: {cm.Year} at bank: {cm.Bank}");
 
+			const int bank = 4;
 			var fm = new FileManager();
 			if (cm.Year.Equals("test"))
 			{
-				fm.Init(cm.Year, cm.Bank, cm.Flag);
+				fm.Init(cm.Year, bank, cm.Flag);
 			}
 			else
 			{
-				fm.Init(cm.Year, cm.Bank2, cm.Flag);
+				fm.Init(cm.Year, cm.Bank, cm.Flag);
 			}
 			
 			//fm.Convert();
-			fm.ProcessAll(cm.Year, cm.Bank, cm.Bank2, cm.Flag);
+			fm.ProcessAll(cm.Year, bank, cm.Bank, cm.Flag);
 
 			Console.WriteLine("Press [ RETURN ]");
 			Console.Read();
