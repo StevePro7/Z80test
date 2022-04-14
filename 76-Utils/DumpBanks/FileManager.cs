cf43a9802190acc13c9fbc2bf07dfc2a251893a7
StevePro7 <steveprozune@hotmail.com>
2022-04-14 16:29:42 +0100

    no message

---
+++ 76-Utils/DumpBanks/FileManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace BinaryFileWrite
+{
+    public class FileManager
+    {
+		public FileManager()
+		{
+		}
+
+		public void Process()
+		{
+			var lines = new List<string>();
+			var years = new int[] { 1978, 1979, 1980, 1981, 1982, 1984, 1986, 1988, 1991, 1995, 1998, 2012 };
+			//var albums = 12;
+			var bank = 16;
+			var text = $"const unsigned char Riff_{0}_0{1}_wav_pcmenc[] = { 0 };";
+
+			for (int index = 0; index < 12; index++)
+			{
+				for (int loop = 0; loop < 4; loop++)
+				{
+					lines.Clear();
+					var album = years[index];
+					var suff = "{ 0 };";
+					var line = $@"const unsigned char Riff_{album}_0{(loop + 1)}_wav_pcmenc[] = " + suff;
+					lines.Add(line);
+
+					var path = $"bank{bank}.c";
+					File.WriteAllLines(path, lines.ToArray());
+					bank++;
+				}
+			}
+
+		}
+    }
+}
---
+++ 76-Utils/DumpBanks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+
+namespace BinaryFileWrite
+{
+	class Program
+	{
+		static void Main()
+		{
+			var fileName = ConfigurationManager.AppSettings["fileName"];
+			var fm = new FileManager();
+			fm.Process();
+            Console.WriteLine("Press [ RETURN ]");
+			Console.Read();
+		}
+
+	}
+}
