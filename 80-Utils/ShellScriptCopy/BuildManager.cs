2e2c126b6f7497c3d964004ce2822d74c4ad519a
StevePro7 <steveprozune@hotmail.com>
2022-04-26 21:15:50 +0100

    no message

---
+++ 80-Utils/ShellScriptCopy/BuildManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SplitWAVpacking
+{	
+    public class BuildManager
+    {
+		public void Process()
+		{
+			var inpLines = File.ReadAllLines("input/build.bat");
+			var outLines = new List<string>();
+		}
+	}
+}
--- 80-Utils/ShellScriptCopy/Program.cs
+++ 80-Utils/ShellScriptCopy/Program.cs
@@ -6,14 +6,8 @@ namespace SplitWAVpacking
 	{
 		static void Main()
 		{
-			var cm = new ConfigManager();
-			cm.Init();
-			Console.WriteLine($"File: {cm.Suff} at bank: {cm.Bank}");
-
-			var fm = new FileManager();
-			fm.Init(cm.Suff, cm.Bank);
-
-			fm.ProcessAll(cm.Suff, cm.Bank);
+			var bm = new BuildManager();
+			bm.Process();
 
 			Console.WriteLine("Press [ RETURN ]");
 			Console.Read();
